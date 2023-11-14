d311fc6af336c87dd3b3a13b15de90a27cdd6697
Omochice <mochice.mls.ntl@gmail.com>
2023-11-14 11:44:19 +0900

    feat: update action

--- denops/ddu-source-redmine/issue/actions/update.ts
+++ denops/ddu-source-redmine/issue/actions/update.ts
@@ -4,7 +4,7 @@ import {
   DduItem,
 } from "https://deno.land/x/ddu_vim@v3.6.0/types.ts";
 import { Denops, fn } from "https://deno.land/x/ddu_vim@v3.6.0/deps.ts";
-import { parse } from "https://deno.land/std@0.206.0/toml/mod.ts";
+import { parse, stringify } from "https://deno.land/std@0.206.0/toml/mod.ts";
 import { define } from "https://deno.land/x/denops_std@v5.0.2/autocmd/mod.ts";
 import { echoerr } from "https://deno.land/x/denops_std@v5.0.2/helper/mod.ts";
 import { register } from "https://deno.land/x/denops_std@v5.0.2/lambda/mod.ts";
@@ -24,7 +24,7 @@ export async function update(args: {
   denops: Denops;
   context: Context;
   items: DduItem[];
-}) {
+}): Promise<ActionFlags> {
   const { denops, items } = args;
   if (items.length !== 1) {
     return ActionFlags.Persist;
@@ -37,16 +37,17 @@ export async function update(args: {
 
   const bufname = `ddu-source-redmine_#${item.issue.id}-update`;
   const bufnr = await prepareBuffer(denops, bufname, bufopts);
-  await fn.setbufline(
-    denops,
-    bufnr,
-    1,
-    [
-      `description = """`,
-      ...(item.issue.description ?? "").split(/\r?\n/),
-      `"""`,
-    ],
-  );
+  try {
+    await fn.setbufline(
+      denops,
+      bufnr,
+      1,
+      stringify(item.issue).split(/\r?\n/),
+    );
+  } catch {
+    echoerr(denops, "Convert Error: the issue cannot convert to toml format");
+    return ActionFlags.None;
+  }
 
   const id = register(denops, async (lines: unknown) => {
     assert(lines, is.ArrayOf(is.String));
