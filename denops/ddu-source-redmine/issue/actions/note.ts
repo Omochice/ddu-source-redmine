a9bf5e78f117bd1054bed4846608add1b84889fd
Omochice <mochice.mls.ntl@gmail.com>
2025-02-21 10:52:43 +0900

    feat!: use markdown filetype for note

--- denops/ddu-source-redmine/issue/actions/note.ts
+++ denops/ddu-source-redmine/issue/actions/note.ts
@@ -1,7 +1,8 @@
 import { ActionFlags, type DduItem } from "jsr:@shougo/ddu-vim@5.0.0/types";
 import type { Denops } from "jsr:@denops/std@7.4.0";
 import * as fn from "jsr:@denops/std@7.4.0/function";
-import { parse, stringify } from "jsr:@std/toml@0.224.0";
+import { stringify } from "jsr:@std/yaml@0.224.0";
+import { extractYaml } from "jsr:@std/front-matter@1.0.7";
 import { define } from "jsr:@denops/std@7.4.0/autocmd";
 import { echoerr } from "jsr:@denops/std@7.4.0/helper";
 import { batch } from "jsr:@denops/std@7.4.0/batch";
@@ -10,28 +11,17 @@ import { format } from "jsr:@denops/std@7.4.0/bufname";
 import { filetype, modified } from "jsr:@denops/std@7.4.0/option";
 import { prepareUnwritableBuffer } from "../prepareBuffer.ts";
 import { update } from "https://deno.land/x/deno_redmine@v0.10.0/issues/update.ts";
-import { assert, is, type Predicate } from "jsr:@core/unknownutil@3.18.1";
+import { assert, is } from "jsr:@core/unknownutil@3.18.1";
 import { isItem } from "../type.ts";
 import { getEditCommand } from "../getEditCommand.ts";
 
-type Note = {
-  notes: string;
+type NoteOption = {
   private_notes?: boolean;
 };
 
-const isNotes = is.ObjectOf({
-  notes: is.String,
-  private_notes: is.OptionalOf(is.Boolean),
-}) satisfies Predicate<Note>;
-
-const convertNote = (note: Note): Required<Note> => {
-  return {
-    notes: note.notes.trim(),
-    private_notes: note.private_notes ?? false,
-  };
-};
-
-const noteTemplate = { notes: "", private_notes: false };
+type Note = {
+  notes: string;
+} & NoteOption;
 
 export async function note(args: {
   denops: Denops;
@@ -56,27 +46,33 @@ export async function note(args: {
   });
   const bufnr = await prepareUnwritableBuffer(denops, bufname);
 
+  const content = [
+    "---",
+    ...stringify({ private_notes: false }).trim().split(/\r?\n/),
+    "---",
+  ];
+
   await batch(denops, async (d) => {
     await fn.setbufline(
       d,
       bufnr,
       1,
-      stringify(noteTemplate).trim().split(/\r?\n/),
+      content,
     );
 
-    await filetype.setBuffer(d, bufnr, "toml");
+    await filetype.setBuffer(d, bufnr, "markdown");
     await modified.setBuffer(d, bufnr, false);
     const id = register(d, async (lines: unknown) => {
       assert(lines, is.ArrayOf(is.String));
       try {
-        const note = parse(lines.join("\n"));
-        if (!isNotes(note)) {
-          await echoerr(d, "Schema is not matched");
-          return;
-        }
-        await update(item.issue.id, convertNote(note), item);
+        const { attrs, body } = extractYaml<NoteOption>(lines.join("\n"));
+        const note = {
+          notes: body.trim(),
+          private_notes: attrs.private_notes ?? false,
+        } satisfies Note;
+        await update(item.issue.id, note, item);
       } catch {
-        await echoerr(d, `Content is invalid toml format: ${lines.join("\n")}`);
+        await echoerr(d, `Content is invalid format: ${lines.join("\n")}`);
       }
     }, { once: true });
 
