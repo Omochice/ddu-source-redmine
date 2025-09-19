def1b81f7b9aab471d11cecdd135d5a02c06055f
Omochice <mochice.mls.ntl@gmail.com>
2025-09-20 00:44:30 +0900

    refactor: add subject type

--- denops/ddu-source-redmine/issue/type.ts
+++ denops/ddu-source-redmine/issue/type.ts
@@ -3,22 +3,26 @@ import { as, is, type Predicate } from "jsr:@core/unknownutil@4.3.0";
 export type Item = {
   issue: {
     id: number;
+    subject: string;
     description: string | undefined;
   };
   endpoint: string;
   apiKey: string;
 };
 
+export const isIssue = is.ObjectOf({
+  id: is.Number,
+  subject: is.String,
+  description: is.UnionOf([
+    is.String,
+    is.Undefined,
+  ]),
+}) satisfies Predicate<Item["issue"]>;
+
 export const isItem = is.ObjectOf({
-  issue: is.ObjectOf({
-    id: is.Number,
-    description: is.UnionOf([
-      is.String,
-      is.Undefined,
-    ]),
-  }),
   endpoint: is.String,
   apiKey: is.String,
+  issue: isIssue,
 }) satisfies Predicate<Item>;
 
 export const mayHasCommand = is.ObjectOf({
